75a256dba5264cccc26dbe1078b182b073432247
CodingCherry <cherry.alexeeva@gmail.com>
2019-02-20 02:06:01 +0700

    Project set up

---
+++ splitter-app/images+Names+Colors.swift
@@ -0,0 +1,28 @@
+//
+//  images+Names+Colors.swift
+//  splitter-app
+//
+//  Created by Alexeeva Ekaterina on 2/20/19.
+//  Copyright © 2019 Alexeeva Ekaterina. All rights reserved.
+//
+
+import UIKit
+
+//images
+
+let camera = UIImage(named: "1")!
+let city = UIImage(named: "2")!
+let buffalo = UIImage(named: "3")!
+let bouqet = UIImage(named: "4")!
+let stand = UIImage(named: "5")!
+let urban = UIImage(named: "6")!
+
+let imageArray = [camera, city, buffalo, bouqet, stand, urban]
+
+//names
+
+let namesArray = ["CAMERA", "CITY", "ANIMALS", "FLOWERS", "STANDS", "URBAN"]
+
+//colors
+
+let colorsArray = [#colorLiteral(red: 0.8078431487, green: 0.02745098062, blue: 0.3333333433, alpha: 1), #colorLiteral(red: 0.2960088602, green: 0.7380781154, blue: 0.9334747779, alpha: 1), #colorLiteral(red: 0.9096734721, green: 1, blue: 0.3938790711, alpha: 1), #colorLiteral(red: 0.9686274529, green: 0.78039217, blue: 0.3450980484, alpha: 1), #colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1), #colorLiteral(red: 0.9372549057, green: 0.3490196168, blue: 0.1921568662, alpha: 1)]
